6d6ef6d795232e318a416a54527bc19b9943c000
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-09-01 12:23:55 -0300

    Implementa módulo de documentação SwaggerUI

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from 'express';
+import swaggerUI from 'swagger-ui-express';
 
 import routes from './routes';
+
+import swaggerDoc from './swagger.json';
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 app.use(routes);
 
 export default app;
