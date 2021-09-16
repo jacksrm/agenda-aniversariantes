51944c043e9b777953d7355cd4dfdc6d5fe4a7d4
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-09-16 19:46:45 -0300

    Adiciona Cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import swaggerUI from 'swagger-ui-express';
 
 import routes from './routes';
@@ -7,6 +8,7 @@ import swaggerDoc from './swagger.json';
 
 const app = express();
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
